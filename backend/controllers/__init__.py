73a55c01c822cd6eda42bd16761761f3f93c4138
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2024-02-25 17:20:04 -0600

    ADD: Restaurant_Get_All

--- backend/controllers/__init__.py
+++ backend/controllers/__init__.py
@@ -6,6 +6,7 @@ from .orders import orders_bp
 from .address import address_bp
 from .register import register_bp
 from .register import login_bp
+from .transaction import transactions_bp
 
 def init_app(app):
     app.register_blueprint(register_bp, url_prefix="/register")
@@ -16,6 +17,7 @@ def init_app(app):
     app.register_blueprint(cart_bp, url_prefix="/cart")
     app.register_blueprint(orders_bp, url_prefix="/orders")
     app.register_blueprint(address_bp, url_prefix="/customerAddresses")
+    app.register_blueprint(transactions_bp, url_prefix="/transaction")
 
     
 
--- backend/controllers/customer.py
+++ backend/controllers/customer.py
@@ -16,16 +16,17 @@ def get_user(id):
     except Exception as e:
         return e
 
-# @customers_bp.route("/", methods=["POST"])
-# def create_user():
-#     try:
-#         postData = request.json
-#         mongo.db.Customers.insert_one(postData)
-#         return {
-#             "message": "User Created Successfully"
-#         }, 200
-#     except Exception as e:
-#         return e
+@customers_bp.route("/", methods=["POST"])
+def create_user():
+    try:
+        postData = request.json
+        postData['role'] = "customer"
+        mongo.db.Customers.insert_one(postData)
+        return {
+            "message": "User Created Successfully"
+        }, 200
+    except Exception as e:
+        return e
 
 @customers_bp.route("/", methods=["PUT"])
 def update_user():
